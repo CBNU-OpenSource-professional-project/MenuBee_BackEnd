621a9992ce3dd00e2db906c80e8fd3da4ac276e0
NKIWI0310 <cjsrhdtktls123@gmail.com>
2023-05-25 16:37:15 +0900

    실행되게 수정

--- src/main/java/menubee_backend/APIKEYS.java
+++ src/main/java/menubee_backend/APIKEYS.java
@@ -3,12 +3,8 @@ package menubee_backend;
 public class APIKEYS {
 
     //올릴때 조심..조심..또 조심..
-<<<<<<< HEAD
     static final String ChatGptAPIKEY = "GPTKEY";
     static final String CloudVisionAPIKEY= "VISIONKEY";
-=======
-//    static final String ChatGptAPIKEY = "근피티";
-//    static final String CloudVisionAPIKEY= "클라우드";
->>>>>>> 0e73c337d0e7481a48c4a80c526561188e1f2a20
+
 
 }
